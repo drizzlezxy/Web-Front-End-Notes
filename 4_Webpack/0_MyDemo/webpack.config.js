a64dcd9a38cdfa5527904a91670a2336b9194602
zhangxiaoyu <drizzlezxy@126.com>
2017-02-05 18:16:09 +0800

    learn webpack

---
+++ 4_Webpack/0_MyDemo/webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require("webpack");
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+module.exports = {
+  entry: {
+    bundle1: './main1.jsx',
+    bundle2: './main2.jsx'
+  },
+  output: {
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js[x]?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader?presets[]=es2015&presets[]=react'
+    }, ]
+  },
+  plugins: [
+    new CommonsChunkPlugin('init.js')
+  ]
+}
